f0da4d9e79a07b8c0e94e8c175727a74b2a2204a
leeandy <leeandy90833@gmail.com>
2022-01-04 15:43:17 +0800

    add mass and inertia sim

--- ICL_controller.m
+++ ICL_controller.m
@@ -17,62 +17,7 @@ record_u = zeros(length(t),1);
 
 
 k1 = 6;
-a = 3;
-x = 60;
-u = 0;
-theta_hat_1 = 5;
-gamma = 3;
-x_before = 0;
-kcl = 4;
-% The adaptive control 
-
-for i=1:length(t)
-    theta = a*a;
-    xd = sin(4*i*dt);
-    xd_dot = 4*cos(4*i*dt);
-    
-    dynamics_update = x - x_before;
-    x_before = x;
-
-    % Regression Matrix
-    Y = sin(x);
-    
-    record_X(i) = x;
-    record_Xd(i) = xd;
-    record_x(i) = dynamics_update;
-    record_theta_hat(i) = theta_hat_1;
-    record_theta(i) = theta;
-
-    e = x - xd;
-
-    record_u(i) = u*dt;
-    record_Y(i) = Y*dt;
-    icl_term = 0;
-
-    % when time > 5s (0.01*500), ICL starts to work
-    if i > 100
-        % Assume N = 100
-        for j = 1:100
-            icl_term = icl_term + record_Y(i-j)*(record_x(i-j) - record_u(i-j) - record_Y(i-j)*theta_hat_1);
-        end
-        
-    end
-
-    % estimate parameters
-    theta_hat_dot_1 = gamma*Y*e + kcl*gamma*icl_term;
-    theta_hat_1 = theta_hat_1 + theta_hat_dot_1*dt;
-
-    % control input
-    u = xd_dot - k1*e - Y*theta_hat_1;
-
-    x_dot = theta*Y + u;
-    x = x + x_dot*dt;
-
-end
-
-
-k1 = 6;
-a = 9;
+a = 8;
 x = 0;
 u = 0;
 theta_hat_1 = 5;
@@ -83,8 +28,8 @@ kcl = 4;
 
 for i=1:length(t)
     theta = a*a;
-    xd = sin(4*i*dt);
-    xd_dot = 4*cos(4*i*dt);
+    xd = sin(i*dt);
+    xd_dot = cos(i*dt);
     
     dynamics_update = x - x_before;
     x_before = x;
@@ -107,7 +52,7 @@ for i=1:length(t)
     % when time > 5s (0.01*500), ICL starts to work
     if i > 100
         % Assume N = 100
-        for j = 1:100
+        for j = 1:10
             icl_term = icl_term + record_Y(i-j)*(record_x(i-j) - record_u(i-j) - record_Y(i-j)*theta_hat_1);
         end
         
@@ -139,6 +84,3 @@ nexttile
 plot(t,record_theta,t,record_theta_hat);
 title("Theta");
 legend('Theta','Theta Hat','Location','southeast')
- 
-
-
