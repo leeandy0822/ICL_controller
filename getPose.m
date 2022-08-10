cb339dba43b8624c8281225192ff9d7c4c598086
leeandy <leeandy@gmail.com>
2022-08-10 18:24:04 +0800

    make caculation faster

--- getPose.m
+++ getPose.m
@@ -1,80 +1,52 @@
 function [uav1,uav2,uav3,uav4,payload] = getPose(uav1,uav2,uav3,uav4,payload)
 
     % subscribers get data
-    pose_payload_data = receive(payload.sub,10);
-    pose_uav1_data = receive(uav1.sub,10);
-    pose_uav2_data = receive(uav2.sub,10);
-    pose_uav3_data = receive(uav3.sub,10);
-    pose_uav4_data = receive(uav4.sub,10);
-
+    tic
+    pose_payload_data = receive(payload.sub,1);
+    pose_uav1_data = receive(uav1.sub,1);
+    pose_uav2_data = receive(uav2.sub,1);
+    pose_uav3_data = receive(uav3.sub,1);
+    pose_uav4_data = receive(uav4.sub,1);
+    toc
     %% Get payload position and orientation
-    payload.pos_x = pose_payload_data.Pose.Pose.Position.X;
-    payload.pos_y = pose_payload_data.Pose.Pose.Position.Y;
-    payload.pos_z = pose_payload_data.Pose.Pose.Position.Z;
-    payload.q_x = pose_payload_data.Pose.Pose.Orientation.X;
-    payload.q_y = pose_payload_data.Pose.Pose.Orientation.Y;
-    payload.q_z = pose_payload_data.Pose.Pose.Orientation.Z;
-    payload.q_w = pose_payload_data.Pose.Pose.Orientation.W;
-    payload.q = [payload.q_w payload.q_x payload.q_y payload.q_z];
-
-    payload.pos = [payload.pos_x payload.pos_y payload.pos_z];
+    tic
+    payload.q = [pose_payload_data.Pose.Pose.Orientation.W pose_payload_data.Pose.Pose.Orientation.X pose_payload_data.Pose.Pose.Orientation.Y pose_payload_data.Pose.Pose.Orientation.Z];
+    payload.pos = [pose_payload_data.Pose.Pose.Position.X pose_payload_data.Pose.Pose.Position.Y pose_payload_data.Pose.Pose.Position.Z];
     payload.R = quat2rotm(payload.q);
     payload.eul = quat2eul(payload.q);
-
-
+    toc
     %% Get UAV1 payload position and orientation
-    uav1.pos_x = pose_uav1_data.Position.X;
-    uav1.pos_y = pose_uav1_data.Position.Y;
-    uav1.pos_z = pose_uav1_data.Position.Z;
-    uav1.q_x = pose_uav1_data.Orientation.X;
-    uav1.q_y = pose_uav1_data.Orientation.Y;
-    uav1.q_z = pose_uav1_data.Orientation.Z;
-    uav1.q_w = pose_uav1_data.Orientation.W;
-
-    uav1.q = [uav1.q_w uav1.q_x uav1.q_y uav1.q_z];
-    uav1.pos = [uav1.pos_x uav1.pos_y uav1.pos_z];
+    uav1.q = [pose_uav1_data.Orientation.W pose_uav1_data.Orientation.X pose_uav1_data.Orientation.Y pose_uav1_data.Orientation.Z];
+%     uav1.pos = [uav1.pos_x uav1.pos_y uav1.pos_z];
     uav1.R = quat2rotm(uav1.q);
     uav1.eul = quat2eul(uav1.q);
 
     %% Get UAV2 payload position and orientation
-    uav2.pos_x = pose_uav2_data.Position.X;
-    uav2.pos_y = pose_uav2_data.Position.Y;
-    uav2.pos_z = pose_uav2_data.Position.Z;
-    uav2.q_x = pose_uav2_data.Orientation.X;
-    uav2.q_y = pose_uav2_data.Orientation.Y;
-    uav2.q_z = pose_uav2_data.Orientation.Z;
-    uav2.q_w = pose_uav2_data.Orientation.W;
+%     uav2.pos_x = pose_uav2_data.Position.X;
+%     uav2.pos_y = pose_uav2_data.Position.Y;
+%     uav2.pos_z = pose_uav2_data.Position.Z;
+    uav2.q = [pose_uav2_data.Orientation.W pose_uav2_data.Orientation.X pose_uav2_data.Orientation.Y pose_uav2_data.Orientation.Z];
 
-    uav2.q = [uav2.q_w uav2.q_x uav2.q_y uav2.q_z];
-    uav2.pos = [uav2.pos_x uav2.pos_y uav2.pos_z];
+%     uav2.pos = [uav2.pos_x uav2.pos_y uav2.pos_z];
     uav2.R = quat2rotm(uav2.q);
     uav2.eul = quat2eul(uav2.q);
 
     %% Get UAV3 payload position and orientation
-    uav3.pos_x = pose_uav3_data.Position.X;
-    uav3.pos_y = pose_uav3_data.Position.Y;
-    uav3.pos_z = pose_uav3_data.Position.Z;
-    uav3.q_x = pose_uav3_data.Orientation.X;
-    uav3.q_y = pose_uav3_data.Orientation.Y;
-    uav3.q_z = pose_uav3_data.Orientation.Z;
-    uav3.q_w = pose_uav3_data.Orientation.W;
+%     uav3.pos_x = pose_uav3_data.Position.X;
+%     uav3.pos_y = pose_uav3_data.Position.Y;
+%     uav3.pos_z = pose_uav3_data.Position.Z;
+    uav3.q = [pose_uav3_data.Orientation.W pose_uav3_data.Orientation.X pose_uav3_data.Orientation.Y pose_uav3_data.Orientation.Z];
 
-    uav3.q = [uav3.q_w uav3.q_x uav3.q_y uav3.q_z];
-    uav3.pos = [uav3.pos_x uav3.pos_y uav3.pos_z];
+%     uav3.pos = [uav3.pos_x uav3.pos_y uav3.pos_z];
     uav3.R = quat2rotm(uav3.q);
     uav3.eul = quat2eul(uav3.q);
 
     %% Get UAV4 payload position and orientation
-    uav4.pos_x = pose_uav4_data.Position.X;
-    uav4.pos_y = pose_uav4_data.Position.Y;
-    uav4.pos_z = pose_uav4_data.Position.Z;
-    uav4.q_x = pose_uav4_data.Orientation.X;
-    uav4.q_y = pose_uav4_data.Orientation.Y;
-    uav4.q_z = pose_uav4_data.Orientation.Z;
-    uav4.q_w = pose_uav4_data.Orientation.W;
-
-    uav4.q = [uav4.q_w uav4.q_x uav4.q_y uav4.q_z];
-    uav4.pos = [uav4.pos_x uav4.pos_y uav4.pos_z];
+%     uav4.pos_x = pose_uav4_data.Position.X;
+%     uav4.pos_y = pose_uav4_data.Position.Y;
+%     uav4.pos_z = pose_uav4_data.Position.Z;
+    uav4.q = [pose_uav4_data.Orientation.W pose_uav4_data.Orientation.X pose_uav4_data.Orientation.Y pose_uav4_data.Orientation.Z];
+%     uav4.pos = [uav4.pos_x uav4.pos_y uav4.pos_z];
     uav4.R = quat2rotm(uav4.q);
     uav4.eul = quat2eul(uav4.q);
 end
--- ros_test.m
+++ ros_test.m
@@ -9,8 +9,6 @@ uav2 = gazebo_dynamics;
 uav3 = gazebo_dynamics;
 uav4 = gazebo_dynamics;
 
-% --- now launch the gazebo files ---
-
 %% initialize the subscriber
 current_time = rostime("now");
 payload.t = current_time.Sec + current_time.Nsec/1000000000;
@@ -25,22 +23,28 @@ uav4.sub = rossubscriber("/firefly4/ground_truth/pose","DataFormat","struct");
 [uav3.pub, uav3.msg] = rospublisher("/firefly3/command/roll_pitch_yawrate_thrust","mav_msgs/RollPitchYawrateThrust");
 [uav4.pub, uav4.msg] = rospublisher("/firefly4/command/roll_pitch_yawrate_thrust","mav_msgs/RollPitchYawrateThrust");
 
-pause(0.5);
 
+u1 = [-2 0 10];
+u2 = [2 0 10];
+u3 = [0 3 10];
+u4 = [0 4 10];
 
-[uav1, uav2, uav3, uav4, payload] = getPose(uav1,uav2,uav3,uav4,payload); 
-
-u1 = [4 0 20];
-u2 = [-4 0 20];
 while true
-force_to_uav(u1,uav1,payload);
-force_to_uav(u2,uav2,payload);
+tic
+[uav1, uav2, uav3, uav4, payload] = getPose(uav1,uav2,uav3,uav4,payload); 
+toc
 
+tic
+force_to_uav(u1,uav1,payload);
+toc
+force_to_uav(u2,uav2,payload);
+force_to_uav(u3,uav3,payload);
+force_to_uav(u4,uav4,payload);
 end
 
 
+
 function force_to_uav(u,uav,payload)
-    
     thrust = rosmessage('geometry_msgs/Vector3');
     yaw_R = rotz(rad2deg(uav.eul(1)));
     force_temp = 1/norm(u)*yaw_R'*payload.R*u';
